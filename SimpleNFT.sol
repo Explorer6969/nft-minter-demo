589f429a34df7914041b715057cc7d9ec51574c5
Explorer6969 <160877141+Explorer6969@users.noreply.github.com>
2025-09-04 10:55:24 +0530

    Create SimpleNFT.sol

---
+++ SimpleNFT.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract SimpleNFT is ERC721URIStorage {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("SimpleNFT", "SNFT") {
+        tokenCounter = 0;
+    }
+
+    function mintNFT(string memory tokenURI) public returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter += 1;
+        return newTokenId;
+    }
+}
